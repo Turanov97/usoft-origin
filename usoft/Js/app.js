c2546f0a89f079920e2efa542eee6ceb8641297c
turanov97 <turanovbekzod116@gmail.com>
2021-12-10 12:40:03 +0500

    infinte loading

--- usoft/Js/app.js
+++ usoft/Js/app.js
@@ -239,3 +239,28 @@ function readURL(input) {
     reader.readAsDataURL(input.files[0]);
   }
 }
+/* lazy loading scrolll=-============ */
+
+window.ias = new InfiniteAjaxScroll('.surface-container', {
+  item: '.article',
+  next: '.pager__next',
+  pagination: '.pager',
+  spinner: '.loader'
+});
+
+ias.on('last', function() {
+  let el = document.querySelector('.no-more');
+
+  el.style.opacity = '1';
+});
+
+// update title and url then scrolling through pages
+ias.on('page', (e) => {
+  document.title = e.title;
+
+  let state = history.state;
+
+  history.replaceState(state, e.title, e.url);
+});
+
+/* lazy loading scrolll=-============  end*/
